bcaf058b9f1fdbab29be8b26cb1b4d45081b9817
Shahules786 <Shahules786@gmail.com>
2023-06-09 01:32:49 +0530

    remove formatting

--- funtuner/utils.py
+++ funtuner/utils.py
@@ -5,25 +5,11 @@ from transformers.models.auto.modeling_auto import (
     MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING,
 )
 
-MODEL_MAPPINGS = [MODEL_FOR_CAUSAL_LM_MAPPING, MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING]
-
-SPECIAL_TOKENS = {"context": "<|prompter|>", "response": "<|assistant|>"}
-
-
-def format_history(context, eos_token):
-    conversation = [
-        "{}{}{}".format(
-            SPECIAL_TOKENS["context" if i % 2 == 0 else "response"],
-            context[i],
-            eos_token,
-        )
-        for i in range(len(context))
-    ]
-    return conversation
+MODEL_MAPPINGS = [MODEL_FOR_CAUSAL_LM_MAPPING]
 
 
 def get_tokenizer(config):
-    tokenizer = AutoTokenizer.from_pretrained(config.model, truncation_side="left")
+    tokenizer = AutoTokenizer.from_pretrained(config.model)
 
     if hasattr(config, "per_digit_tokens") and config.per_digit_tokens:
         tokenizer._tokenizer.pre_processor = pre_tokenizers.Digits(True)
@@ -35,8 +21,6 @@ def get_tokenizer(config):
             "sep_token": config.special_tokens.sep_token,
         }
         tokenizer.add_special_tokens(special_tokens)
-
-    tokenizer.truncation_side = "left"
 
     return tokenizer
 
