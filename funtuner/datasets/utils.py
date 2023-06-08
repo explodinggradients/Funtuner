c0b41202df02e2548f033c4fb9e923757a4999a9
Shahules786 <Shahules786@gmail.com>
2023-06-09 01:33:56 +0530

    fix multi dataset loading

--- funtuner/datasets/utils.py
+++ funtuner/datasets/utils.py
@@ -1,25 +1,44 @@
 import torch
 from torch.utils.data import random_split, ConcatDataset
-from funtuner.datasets.dolly import DollyDataset
+from funtuner.datasets.sftdataset import SFTDataset
+import yaml
+from pathlib import Path
 
 generator = torch.Generator().manual_seed(42)
 
 
-def get_single_dataset(name, **kwargs):
-    if name == "dolly":
-        dataset = DollyDataset(**kwargs)
+SPECIAL_TOKENS = {
+    "prompt": "<|prompter|>",
+    "response": "<|assistant|>",
+    "context": "<|context|>",
+}
+
+DATASET_MAPPING = yaml.safe_load(Path('funtuner/config/datasets.yaml').read_text())
+
+
+def get_single_dataset(name, split, sep_token):
+    args = DATASET_MAPPING.get(name)
+    if args is not None:
+        dataset = SFTDataset(name=name, split=split, sep_token=sep_token)
     else:
-        raise ValueError(f"Invalid dataset name {name}")
+        raise ValueError(f"Invalid dataset name {name}. Add dataset to dataset.yaml")
 
     return dataset
 
 
 def get_datasets(config):
     dataset_list = []
+    sep_token = config.special_tokens.sep_token
     for dataset in config.datasets:
-        name = list(dataset.keys())[0]
-        kwargs = dataset[name]
-        dataset_list.append(get_single_dataset(name, **kwargs))
+        name = dataset["name"]
+        splits = dataset["split"]
+        dataset_list.append(
+            get_single_dataset(
+                name,
+                splits,
+                sep_token,
+            )
+        )
 
     dataset = ConcatDataset(dataset_list)
     train_dataset, valid_dataset = random_split(
