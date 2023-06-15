4425f70addebfd57066c6b6543335102d613a1c9
Shahules786 <Shahules786@gmail.com>
2023-06-15 10:02:00 +0530

    requires grad fix

--- funtuner/trainer.py
+++ funtuner/trainer.py
@@ -52,6 +52,14 @@ def train(cfg: DictConfig) -> None:
     tokenizer = get_tokenizer(cfg)
     model.resize_token_embeddings(len(tokenizer))
     
+    if hasattr(model, "enable_input_require_grads"):
+    model.enable_input_require_grads()
+    else:
+        def make_inputs_require_grad(module, input, output):
+            output.requires_grad_(True)
+
+        model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
+    
     if cfg.eight_bit_training:
         model = prepare_model_for_int8_training(model)
 
