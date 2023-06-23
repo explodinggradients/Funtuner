3b492300fe873843f8eda22489082b0b852bc90b
Shahules786 <Shahules786@gmail.com>
2023-06-23 21:25:08 +0530

    fix sampling

--- funtuner/sampling.py
+++ funtuner/sampling.py
@@ -1,10 +1,14 @@
+import itertools
 from funtuner.inference import Inference
+from funtuner.custom_datasets.utils import DATASET_MAPPING
 from datasets import load_dataset
 import argparse
 
-def sampling(examples, model, generation_args):
+def sampling(examples, model, generation_args, dataset):
     
-    examples = [[example["instruction"],example["input"]] for example in examples]
+    dataset = DATASET_MAPPING[dataset]
+    instruction, input = dataset.get("prompt"), dataset.get("context",[None])
+    examples =  list(itertools.zip_longest(instruction, input))
     output = model.batch_generate(examples, **generation_args)
     return {"completion": output}
     
@@ -14,12 +18,14 @@ def sampling(examples, model, generation_args):
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--model_url", type=str, default="shahules786/GPTNeo-125M-lora", help="model name")
-    parser.add_argument("--dataset", type=str, default="vicgalle/alpaca-gpt4", help="dataset name")
-    parser.add_argument("--num_samples", type=int, default=100, help="num of samples to run inference")
-    parser.add_argument("--hf_upload", type=int, default="shahues786/test-results", help="hf repo")
+    parser.add_argument("--model_url", type=str, help="model name")
+    parser.add_argument("--dataset", type=str, help="dataset name")
+    parser.add_argument("--split", type=str, default="test", help="dataset split")
 
-    parser.add_argument("--batch_size", type=int, default=100, help="")
+    parser.add_argument("--num_samples", type=int, default=100, help="num of samples to run inference")
+    parser.add_argument("--save_path", type=str, default="results.json", help="save path")
+
+    parser.add_argument("--batch_size", type=int, default=4, help="")
     parser.add_argument("--temperature", type=float, default=0.1, help="")
     parser.add_argument("--top_p", type=float, default=0.75, help="")
     parser.add_argument("--top_k", type=int, default=40, help="")
@@ -32,9 +38,9 @@ if __name__ == "__main__":
     args = parser.parse_args().__dict__
     
     generation_args = {k: args.get(k) for k in generation_args}
-    model = Inference(args.get("model"))
-    dataset = load_dataset(args.get("dataset"), split="train").select(range(0, 100))
-    dataset = dataset.map(lambda batch: sampling(batch, model, generation_args), batch_size=args.get("batch_size"))
-    dataset.push_to_hub(args.get("hf_upload"))
+    model = Inference(args.get("model_url"))
+    dataset = load_dataset(args.get("dataset"), split=args.get("split")).select(range(0, 100))
+    dataset = dataset.map(lambda batch: sampling(batch, model, generation_args, args.get("dataset")), batch_size=args.get("batch_size"), batched=True)
+    dataset.to_json(args.get("save_path"), indent=4)
 
     
